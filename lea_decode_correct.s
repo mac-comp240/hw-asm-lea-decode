af282ee66578efb6c7ef2c605763c247c8f7baea
Abigail Marsh <amarsh1@macalester.edu>
2019-09-28 11:46:42 -0500

    updated sol'n for gcc 8.3

--- lea_decode_correct.s
+++ lea_decode_correct.s
@@ -1,19 +1,19 @@
-	.file	"lea_decode.c"
-	.text
-	.globl	decode
-	.type	decode, @function
+.file   "lea_decode.c"
+        .text
+        .globl  decode
+        .type   decode, @function
 decode:
 .LFB0:
-	.cfi_startproc
-	leaq	(%rsi,%rsi,8), %rax
-	movq	%rdi, %rdx
-	salq	$4, %rdx
-	negq	%rdx
-	addq	%rdi, %rdx
-	addq	%rdx, %rax
-	ret
-	.cfi_endproc
+        .cfi_startproc
+        leaq    (%rsi,%rsi,8), %rax
+        movq    %rdi, %rdx
+        salq    $4, %rdx
+        subq    %rdx, %rdi
+        addq    %rdi, %rax
+        ret
+        .cfi_endproc
 .LFE0:
-	.size	decode, .-decode
-	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4"
-	.section	.note.GNU-stack,"",@progbits
+        .size   decode, .-decode
+        .ident  "GCC: (Ubuntu 8.3.0-6ubuntu1) 8.3.0"
+        .section        .note.GNU-stack,"",@progbits
+
